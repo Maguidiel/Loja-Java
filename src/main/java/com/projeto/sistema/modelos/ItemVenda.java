45dfc9d2bf5ae9c6712891bd96cb9083818a7cb9
User <User@DESKTOP-UF3683O>
2025-01-25 23:02:03 -0300

    Commit

---
+++ src/main/java/com/projeto/sistema/modelos/ItemVenda.java
@@ -0,0 +1,67 @@
+package com.projeto.sistema.modelos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+public class ItemVenda implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Double quantidade;
+	private Double valor;
+	private Double subtotal;
+	@ManyToOne
+	private Venda venda;
+	@ManyToOne
+	private Produto produto;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Double getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Double quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public Double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+	public Venda getVenda() {
+		return venda;
+	}
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
---
+++ src/main/java/com/projeto/sistema/repositorios/ItemVendaRepositorio.java
@@ -0,0 +1,15 @@
+package com.projeto.sistema.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.sistema.modelos.ItemVenda;
+
+public interface ItemVendaRepositorio extends JpaRepository<ItemVenda, Long> {
+
+	@Query("SELECT e FROM ItemVenda e WHERE e.venda.id = ?1")
+	List<ItemVenda>buscarPorVenda(long id);
+	
+}
